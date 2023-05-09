5cb7b42a609d21041d9b89269e33ec31d6098909
unknown <rafael.bidetti@gmail.com>
2023-05-09 18:56:05 -0300

    Protótipo Final - Calculadora React
    Todos os botões funcionando

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #D13E1E;
+    color: black;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
